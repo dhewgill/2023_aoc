573cfd811fd76307fc4ea6e4a9e6828d8e37b9de
Dale Hewgill <dhewgill@gmail.com>
2023-12-01 11:59:29 -0800

    Fix up some formatting in day 1.

--- day01.py
+++ day01.py
@@ -16,14 +16,14 @@ P2_DATFILE = P1_DATFILE #r".\data\d1p2_eg.dat"
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--log-lvl", dest="log_lvl", type=str, default="INFO", help="Logging level.")
     args = parser.parse_args()
     log_lvl = args.log_lvl
-    
+
     logging.basicConfig(level=log_lvl)
-    
+
     print("Start\n")
 
     # Part 1.
@@ -46,13 +46,13 @@ def get_p1_line_score(input_line: str) -> int:
         if c.isdigit():
             digit += c
             break
-    
+
     # Find the last digit.
     for c in input_line[::-1]:
         if c.isdigit():
             digit += c
             break
-    
+
     return int(digit)
 
 
@@ -68,7 +68,7 @@ def get_p2_line_score(input_line: str) -> int:
     )
     tokens_map = {t: f"{i}" for i, t in enumerate(tokens, 1)}
     digit = ""
-    
+
     # Try to find the each token in the string and track the index they are found in.
     left_tok_track = dict()
     right_tok_track = dict()
@@ -77,19 +77,19 @@ def get_p2_line_score(input_line: str) -> int:
         left_indx = input_line.find(tok)
         if left_indx > -1:
             left_tok_track[left_indx] = tok
-        
+
         right_indx = input_line.rfind(tok)
         if right_indx > -1:
             right_tok_track[right_indx] = tok
-    
+
     # First digit.
     first_digit = left_tok_track[min(left_tok_track)]
-    
+
     if first_digit.isdigit():
         digit += first_digit
     else:
         digit += tokens_map[first_digit]
-    
+
     # Last digit.
     last_digit = right_tok_track[max(right_tok_track)]
 
